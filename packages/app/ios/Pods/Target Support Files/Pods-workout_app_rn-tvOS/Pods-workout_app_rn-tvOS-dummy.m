d18c9f009e80a3e3ce32f285cd07862a79dbdcbe
Andrea <andrea.belluccia@gmail.com>
2019-07-05 15:29:43 +0200

    first commit

---
+++ packages/app/ios/Pods/Target Support Files/Pods-workout_app_rn-tvOS/Pods-workout_app_rn-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_workout_app_rn_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_workout_app_rn_tvOS
+@end
---
+++ packages/app/ios/Pods/Target Support Files/Pods-workout_app_rn-tvOSTests/Pods-workout_app_rn-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_workout_app_rn_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_workout_app_rn_tvOSTests
+@end
